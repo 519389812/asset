2dd1a0f500380d8700b1edda1f10eda70be90463
519389812 <519389812>
2020-05-29 17:53:25 +0800

    29may20 01

--- current/admin.py
+++ current/admin.py
@@ -7,5 +7,11 @@ class CurrentAdmin(admin.ModelAdmin):
     list_display_links = ("id",)
     search_fields = ("name",)
 
+    def get_readonly_fields(self, request, obj=None):
+        if obj is not None:
+            return ["id", "name"]
+        else:
+            return []
+
 
 admin.site.register(Current, CurrentAdmin)
--- place/admin.py
+++ place/admin.py
@@ -9,11 +9,23 @@ class RoomAdmin(admin.ModelAdmin):
     list_display_links = ("id",)
     search_fields = ("name",)
 
+    def get_readonly_fields(self, request, obj=None):
+        if obj is not None:
+            return ["id", "name"]
+        else:
+            return []
+
 
 class AreaAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "room",)
     list_display_links = ("id",)
     search_fields = ("name",)
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj is not None:
+            return ["id", "name"]
+        else:
+            return []
 
     def save_model(self, request, obj, form, change):
         if change:
